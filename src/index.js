aeaf8456e209a0b7936d0398df60242209176092
Dimitar Ivanov <dimitucha2004@gmail.com>
2025-09-16 14:07:58 +0300

    Read home html from file system

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import http from "http";
-
-import homeHTML from "./home.html.js";
 import homeCSS from "./home.css.js";
+import fs from "fs/promises";
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   if (req.url === "/") {
+    const homeHTML = await fs.readFile("./src/views/home/index.html", {encoding: "utf-8"});
+    
     res.writeHead(200, {
       "content-type": "text/html",
     });
+    
     res.write(homeHTML);
   } else if (req.url === "/styles/site.css") {
     res.writeHead(200, {
