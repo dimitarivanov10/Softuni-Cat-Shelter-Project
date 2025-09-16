53de412be99daf412f78b618bf321d67a2a4ca11
Dimitar Ivanov <dimitucha2004@gmail.com>
2025-09-16 14:41:30 +0300

    Add create cat page

--- src/index.js
+++ src/index.js
@@ -2,25 +2,49 @@ import http from "http";
 import fs from "fs/promises";
 
 const server = http.createServer(async (req, res) => {
-  if (req.url === "/") {
-    const homeHTML = await fs.readFile("./src/views/home/index.html", {encoding: "utf-8"});
-    
-    res.writeHead(200, {
-      "content-type": "text/html",
-    });
-    
-    res.write(homeHTML);
-  } else if (req.url === "/styles/site.css") {
-    const sitesCSS = await fs.readFile("./src/styles/site.css", {encoding: "utf-8"});
-    res.writeHead(200, {
-      "content-type": "text/css",
-    });
-
-    res.write(sitesCSS);
+  let html;
+  switch (req.url) {
+    case "/":
+      html = await homeView();
+      break;
+    case "/cats/add-breed":
+      html = await addBreedView();
+      break;
+    case "/styles/site.css":
+      const sitesCSS = await fs.readFile("./src/styles/site.css", {
+        encoding: "utf-8",
+      });
+      res.writeHead(200, {
+        "content-type": "text/css",
+      });
+      res.write(sitesCSS);
+      res.end();
+      return;
+    default:
+      return res.end();
   }
+  res.writeHead(200, {
+    "content-type": "text/html",
+  });
+
+  res.write(html);
 
   res.end();
 });
+
+async function homeView() {
+  const html = await fs.readFile("./src/views/home/index.html", {
+    encoding: "utf-8",
+  });
+  return html;
+}
+
+async function addBreedView() {
+  const html = await fs.readFile("./src/views/addBreed.html", {
+    encoding: "utf-8",
+  });
+  return html;
+}
 server.listen(5000);
 
 console.log("Server is listening on http://localhost:5000...");
