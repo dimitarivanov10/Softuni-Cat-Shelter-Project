395b7b9802333fb691cca7b64bfd4f15b000046e
Dimitar Ivanov <dimitucha2004@gmail.com>
2025-09-16 13:02:49 +0300

    Add basic http server

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-console.log("Champiossns")
-console.log("Champiossns")
-console.log("Champion")
-console.log("Champiossns")
+import http from "http";
+
+const server = http.createServer((req, res) => {
+    res.write("Working!");
+
+    res.end();
+});
+server.listen(5000);
+
+console.log("Server is listening on http://localhost:5000...");
