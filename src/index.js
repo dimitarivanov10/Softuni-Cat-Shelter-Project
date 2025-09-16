afe65f9a8a87286d8da356c381b6c5e22104e3b4
Dimitar Ivanov <dimitucha2004@gmail.com>
2025-09-16 17:02:42 +0300

    Add render view function

--- src/index.js
+++ src/index.js
@@ -35,24 +35,22 @@ const server = http.createServer(async (req, res) => {
   res.end();
 });
 
+function renderView(path){
+    return fs.readFile(path, {encoding: "utf-8"});
+}
+
 async function homeView() {
-  const html = await fs.readFile("./src/views/home/index.html", {
-    encoding: "utf-8",
-  });
+  const html = await renderView("./src/views/home/index.html");
   return html;
 }
 
 async function addBreedView() {
-  const html = await fs.readFile("./src/views/addBreed.html", {
-    encoding: "utf-8",
-  });
+  const html = await renderView("./src/views/addBreed.html");
   return html;
 }
 
 async function addCatView() {
-  const html = await fs.readFile("./src/views/addCat.html", {
-    encoding: "utf-8",
-  });
+  const html = await renderView("./src/views/addCat.html");
   return html;
 }
 server.listen(5000);
